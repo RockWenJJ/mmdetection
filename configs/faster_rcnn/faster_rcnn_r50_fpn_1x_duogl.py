b1180e685f8a3ce8d01174a0499f6a4e42f512f2
wenjj <jjwen@mae.cuhk.edu.hk>
2022-09-22 11:04:04 +0800

    fix the bug when training duogl dataset
    Signed-off-by: wenjj <jjwen@mae.cuhk.edu.hk>

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_duogl.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_duogl.py
@@ -1,6 +1,24 @@
 _base_ = ['./faster_rcnn_r50_fpn_1x_duor.py']
 
 data_root = 'data/duo_glnet/'
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=0,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train.json',
+        img_prefix=data_root + '/images/train/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_test.json',
+        img_prefix=data_root + 'images/test/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_test.json',
+        img_prefix=data_root + 'images/test/',
+        pipeline=test_pipeline))
 
 log_config = dict(
     hooks=[
